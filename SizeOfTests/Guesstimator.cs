f294dc24264eb09b8a951619baea01b4c3da1f1f
Toby Stone <40922682+stickmonster@users.noreply.github.com>
2022-12-20 18:53:09 +0000

    Trying some changes you've prob already thought of...
    Hi Ryan, trying some changes...

--- SizeOfTests/Guesstimator.cs
+++ SizeOfTests/Guesstimator.cs
@@ -40,6 +40,9 @@
                         estimatedSize += sizeof(char) * stringArray.Sum(sa => sa.Length);
                         break;
                     case null: // TODO: How much does a null property take up? The same as the non nullable + overhead?
+                        //NULL normally but 'not always' a pointer to memory location  0- sizeof(pointer) based on architecture type...
+                        //obvs normally either 4 or 8 bytes, at least in C... ps, none of this is probably helpful I'm just trying changing stuff...
+                        estimatedSize = is64BitOperatingSystem || is64BitProcess ? 8 : 4;
                         break;
                     default:
                         // Log so we catch all properties.
